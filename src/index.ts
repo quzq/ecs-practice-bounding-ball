b2e53da5c079b62bda3ea3e354c6219bd5865344
quzq <user@MSI>
2025-02-13 21:33:55 +0900

    add main()

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("hello! developer world");
+const main = (viewport: HTMLCanvasElement): void => {
+  const ctx = viewport.getContext("2d");
+  if (!ctx) return;
+  console.log("Hello, ecs!");
+};
+main(<HTMLCanvasElement>document.getElementById("viewport"));
